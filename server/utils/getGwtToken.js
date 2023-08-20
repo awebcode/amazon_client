6cfb8474b6845333085aac22c89447b243129f9a
Md-Asikur <asikurrahaman997@gamil.com>
2023-08-20 07:26:09 +0600

    a

--- server/utils/getGwtToken.js
+++ server/utils/getGwtToken.js
@@ -4,8 +4,9 @@ const sendToken = (user, statusCode, res) => {
   // Options for cookie
   const options = {
     httpOnly: true,
-    secure: true,
-    // sameSite: "strict",
+    // secure: true,
+    sameSite: "none",
+    // path:"/",
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // Set SameSite attribute for cross-site cookies
   };
 
